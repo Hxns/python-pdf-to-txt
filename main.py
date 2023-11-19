935ee54a72bb5450795c7a2535695942389312d9
Hans Eliot Herzfelder <dev.herzfelder@gmail.com>
2023-11-19 20:22:20 +0100

    Update main.py
    function pdf_to_txt_line_by_line

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import PyPDF2
+
+def pdf_to_txt_line_by_line(pdf_path, txt_path):
+    with open(pdf_path, "rb") as pdf_file:
+        reader = PyPDF2.PdfFileReader(pdf_file)
+
+        with open(txt_path, "w") as txt_file:
+            for page_num in range(reader.numPages):
+                page = reader.getPage(page_num)
+                content = page.extact_text()
+                lines = content.split('\n')
+
+                for line in lines:
+                    txt_file.write(line +'\n')
+
+    print(f"Conversion complete. TXT file saved at: {txt_path}")
